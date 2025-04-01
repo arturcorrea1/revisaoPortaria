9d2f4bff11377ef8a719e7dae19382745e99a439
ARTUR CORREA <04335116055@senacrs.edu.br>
2025-04-01 12:15:19 -0300

    função de cadastro, listagem, deletar feitas, editar em produção.

--- back/src/server.js
+++ back/src/server.js
@@ -9,65 +9,71 @@ app.use(express.json());
 const port = 3000;
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
 
-// app.post("/cadastrar", (req, res) => {
-//   const { placa, dono, cpf, tipo, vaga } = req.body;
+app.post("/cadastrar", (req, res) => {
+    const { nome, telefone, email, tipo, apartamento } = req.body;
+  
+    if ( !nome || !telefone || !email || !tipo || !apartamento) {
+        return res.status(400).json({ success: false, message: "Todos os campos são obrigatórios." });
+    }
 
-//   if (!placa || !dono || !cpf || !tipo || !vaga) {
-//       return res.status(400).json({ success: false, message: "Todos os campos são obrigatórios." });
-//   }
+        // if (results.length > 0) {
+        //     return res.status(400).json({ success: false, message: "ID já registrado" });
+        // }
+  
+        const query = "INSERT INTO moradores (nome, telefone, email, tipo, apartamento) VALUES (?, ?, ?, ?, ?)";
+        connection.query(query, [nome, telefone, email, tipo, apartamento], (err) => {
+            if (err) {
+                console.error("Erro ao cadastrar carro:", err);
+                return res.status(500).json({ success: false, message: "Erro ao cadastrar carro." });
+            }
+            res.json({ success: true, message: "Cadastro realizado com sucesso" });
+        });
+    });
+  
 
-//   connection.query("SELECT * FROM usuario WHERE vaga = ?", [vaga], (err, results) => {
-//       if (err) {
-//           console.error("Erro ao verificar vaga:", err);
-//           return res.status(500).json({ success: false, message: "Erro ao verificar vaga." });
-//       }
+    app.get('/listar', (req, res) => {
+        const query = 'SELECT * FROM moradores';
+        connection.query(query, (err, results) => {
+          if (err) {
+            return res.status(500).json({ success: false, message: 'Erro ao buscar' });
+          }
+          res.json({ success: true, pessoal: results });
+        });
+    
+      });
+      app.delete('/deletar/:id', (req, res) => {
+        const {id} = req.params;
+        const query = 'DELETE FROM moradores WHERE id = ?';
+        connection.query(query, [id], (err) => {
+          if (err) {
+            return res.status(500).json({ success: false, message: 'Erro ao remover' });
+          }
+          res.json({ success: true, message: 'Removido com sucesso!' });
+        });
+      });
 
-//       if (results.length > 0) {
-//           return res.status(400).json({ success: false, message: "Vaga já ocupada." });
-//       }
-
-//       const query = "INSERT INTO usuario (placa, dono, cpf, tipo, vaga) VALUES (?, ?, ?, ?, ?)";
-//       connection.query(query, [placa, dono, cpf, tipo, vaga], (err) => {
-//           if (err) {
-//               console.error("Erro ao cadastrar carro:", err);
-//               return res.status(500).json({ success: false, message: "Erro ao cadastrar carro." });
-//           }
-//           res.json({ success: true, message: "Cadastro realizado com sucesso" });
-//       });
-//   });
-// });
-
-//   app.get('/listar', (req, res) => {
-//     const query = 'SELECT * FROM usuario';
-//     connection.query(query, (err, results) => {
-//       if (err) {
-//         return res.status(500).json({ success: false, message: 'Erro ao buscar carros na garagem.' });
-//       }
-//       res.json({ success: true, carro: results });
-//     });
-
-//   });
-//   app.delete('/excluir/:placa', (req, res) => {
-//     const {placa} = req.params;
-//     const query = 'DELETE FROM usuario WHERE placa = ?';
-//     connection.query(query, [placa], (err) => {
-//       if (err) {
-//         return res.status(500).json({ success: false, message: 'Erro ao remover carro.' });
-//       }
-//       res.json({ success: true, message: 'Carro removido com sucesso!' });
-//     });
-//   });
-
-// app.delete('/deletar/:placa', (req, res) => {
-//     const {placa } = req.params;
-//     const query = 'DELETE FROM usuario WHERE placa = ?';
-//     connection.query(query, [placa], (err) => {
-//       if (err) {
-//         return res.status(500).json({ success: false, message: 'Erro ao remover carro.' });
-//       }
-//       res.json({ success: true, message: 'Carro removido com sucesso!' });
-//     });
-// });
+      app.put('/editar/:id', (req, res) => {
+        const idAntigo = req.params.id;
+        console.log(req.params);
+        const idNovo = req.body.id;
+        const nome = req.body.nome
+        const email = req.body.email
+        const tipo = req.body.tipo
+        const apartamento = req.body.apartamento
+      
+        console.log(req.body);
+        console.log('Recebendo requisição para editar:', { idAntigo, nome, idNovo, email, tipo, apartamento });
+      
+        const query = 'UPDATE moradores SET id=?, nome=?, email=?, tipo=?, apartamento=? WHERE id=?';
+        connection.query(query, [ idAntigo, nome, idNovo, email, tipo, apartamento ], (err) => {
+            if (err) {
+                console.error('Erro no banco de dados:', err);
+                return res.status(500).json({ success: false, message: 'Erro ao atualizar' });
+            }
+            res.json({ success: true, message: 'Atualizado com sucesso!' });
+        });
+      });
+      
 
 // app.put('/editar/:placa', (req, res) => {
 //   const placaAntiga = req.params.placa;
--- front/index.js
+++ front/index.js
@@ -1,27 +1,25 @@
-function updateSizeInfo() {
-    let largura = window.innerWidth
-    document.getElementById("sizeInfo").textContent = `Largura da janela: ${largura}px`
+// function updateSizeInfo() {
+//     let largura = window.innerWidth
+//     document.getElementById("sizeInfo").textContent = `Largura da janela: ${largura}px`
 
-    let container = document.getElementByClass("container")
-    let inputs = document.querySelectorAll("input, select")
+//     let container = document.getElementsByClassName("container")
+//     let inputs = document.querySelectorAll("input, select")
 
-    if (largura < 600) {
-        container.style.backgroundColor = "#222"
-        inputs.forEach(input => {
-            input.style.width = "70%"
-            input.style.padding = "6px"
-        })
-    } else {
-        container.style.backgroundColor = "#333"
-        inputs.forEach(input => {
-            input.style.width = "90%"
-            input.style.padding = "8px"
-        })
-    }
-}
+//     if (largura < 600) {
+//         inputs.forEach(input => {
+//             input.style.width = "70%"
+//             input.style.padding = "6px"
+//         })
+//     } else {
+//         inputs.forEach(input => {
+//             input.style.width = "90%"
+//             input.style.padding = "8px"
+//         })
+//     }
+// }
 
-window.addEventListener("resize", updateSizeInfo)
-updateSizeInfo()
+// window.addEventListener("resize", updateSizeInfo)
+// updateSizeInfo()
 
 // cadastro
 
@@ -39,20 +37,22 @@ async function cadastrar(event){
     const tipo = document.getElementById("tipo").value;
     const apartamento = document.getElementById("apartamento").value;
 
-    console.log(nome, telefone, email, tipo, apartamento);
+    const data = {nome, telefone,email,tipo,apartamento}
+    console.log(data);
+
 
     try {
         const response = await fetch("http://localhost:3000/cadastrar", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({nome, telefone, email, tipo, apartamento}),
+            body: JSON.stringify(data),
         });
 
         const result = await response.json();
 
         if (result.success) {
             alert("Cadastro feito com sucesso");
-            document.getElementById("cadastroForm").reset();
+            // document.getElementById("formulario").reset();
         } else {
             alert(result.message || "Houve um erro ao cadastrar");
         }
@@ -68,9 +68,6 @@ document.addEventListener("DOMContentLoaded", () => {
     listar()
 });
 
-let placaAntiga = "";
-let placaNova = "";
-
   async function listar() {
     const tbody = document.getElementById("tbody");
 
@@ -83,43 +80,46 @@ let placaNova = "";
     const data = await response.json();
     tbody.innerHTML = ""; // Limpa a tabela antes de adicionar os novos itens
 
-    data.carro.forEach((carro) => {
+    data.pessoal.forEach((pessoal) => {
         const row = document.createElement("tr");
         row.innerHTML = `
-            <td>${carro.placa}</td>
-            <td>${carro.tipo}</td>
-            <td>${carro.dono}</td>
-            <td>${carro.cpf}</td>
-            <td>${carro.vaga}</td>
-            <td class="delete-btn" onclick="excluir('${carro.placa}')">✖</td>
-            <td class="editar-btn" onclick="abrirModal('${carro.dono}', '${carro.placa}', '${carro.cpf}', '${carro.tipo}', '${carro.vaga}')">Editar</td>
+            <td>${pessoal.id}</td>
+            <td>${pessoal.nome}</td>
+            <td>${pessoal.tipo}</td>
+            <td>${pessoal.telefone}</td>
+            <td>${pessoal.placa}</td>
+            <td>${pessoal.apartamento}</td>
+            <td class="delete-btn" onclick="excluir('${pessoal.id}')">✖</td>
+            <td class="editar-btn" onclick="abrirModal('${pessoal.nome}', '${pessoal.tipo}', '${pessoal.telefone}', '${pessoal.placa}', '${pessoal.apartamento}')">Editar</td>
         `;
         tbody.appendChild(row);
     });
 }
 
-async function excluir(placa) {
-    const response = await fetch(`http://localhost:3000/deletar/${placa}`, {
+// excluir
+
+async function excluir(id) {
+    const response = await fetch(`http://localhost:3000/deletar/${id}`, {
         method: 'DELETE'
     });
 
     const result = await response.json();
     if (result.success) {
         alert('Carro removido com sucesso!');
-        listarGaragem(); // Recarrega a lista após a exclusão
+        listarPessoal(); // Recarrega a lista após a exclusão
     } else {
         alert(result.message || 'Erro ao remover o carro!');
     }
 }
 
 // Função para abrir o modal e preencher os campos
-function abrirModal(dono, placa, cpf, tipo, vaga, placaAntigaParam) {
-    placaAntiga = placaAntigaParam; 
-    document.getElementById('dono').value = dono;
-    document.getElementById('placa').value = placa;
-    document.getElementById('cpf').value = cpf;
+function abrirModal(nome, telefone, email, tipo, apartamento) {
+    idAntigo = idAntigoParam; 
+    document.getElementById('nome').value = nome;
+    document.getElementById('telefone').value = telefone;
+    document.getElementById('email').value = email;
     document.getElementById('tipo').value = tipo;
-    document.getElementById('vaga').value = vaga;
+    document.getElementById('apartamento').value = apartamento;
 
     // Exibir o modal
     document.getElementById('modal').style.display = 'flex';
@@ -130,21 +130,21 @@ function fecharModal() {
     document.getElementById('modal').style.display = 'none';
 }
 
-async function editar(placaAntiga) {
-    const dono = document.getElementById('nome').value;
-    const placaNova = document.getElementById('telefone').value;
-    const cpf = document.getElementById('email').value;
+async function editar(id) {
+    const nome = document.getElementById('nome').value;
+    const telefone = document.getElementById('telefone').value;
+    const email = document.getElementById('email').value;
     const tipo = document.getElementById('tipo').value;
-    const vaga = document.getElementById('vaga').value;
+    const apartamento = document.getElementById('apartamento').value;
 
-    console.log('Dados enviados:', { dono, placaNova, cpf, tipo, vaga });
+    console.log('Dados enviados:', { nome, telefone, email, tipo, apartamento });
 
-    const response = await fetch(`http://localhost:3000/editar/${placaAntiga}`, {
+    const response = await fetch(`http://localhost:3000/editar/${id}`, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ dono, placaNova, cpf, tipo, vaga })
+        body: JSON.stringify({ nome, telefone, email, tipo, apartamento })
 
     });
 
